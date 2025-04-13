b4bd1a0ff243db6a8d874cd139759560ce672700
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-04-13 15:51:02 -0400

    About section
    Added beginnings of about section, cleaned up some code in the home section

--- client/src/components/Page.vue
+++ client/src/components/Page.vue
@@ -11,16 +11,26 @@ export default {
 
 <template>
   <div id="main-page" class="text-black dark:text-white">
-    <Navbar/>
-    <div id="home" class="flex items-center justify-center h-screen bg-white dark:bg-gray-900">
-      <div class="grid grid-cols-1">
-        <div>
-          <p class="inline-block animate-typewriter w-0 overflow-hidden whitespace-nowrap border-r text-9xl">Hi, I'm Daniel.</p>
-        </div>
-        <div>
+    <Navbar />
+    <div
+      id="home"
+      class="flex items-center justify-center h-screen bg-white dark:bg-gray-900"
+    >
+    <div>
+          <p
+            class="inline-block animate-typewriter w-0 overflow-hidden whitespace-nowrap border-r text-9xl"
+          >
+            Hi, I'm Daniel.
+          </p>
           <p class="text-2xl font-medium">Passionate in software engineering</p>
         </div>
-      </div>
+    </div>
+    <div
+      id="about"
+      class="flex items-center justify-center h-screen bg-white dark:bg-gray-900"
+    >
+      <p class="text-8xl font-medium">About</p>
+      <p class="text-2xl font-medium">I am a software engineer with a passion for building web applications.</p>
     </div>
   </div>
 </template>
