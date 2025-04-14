49e675177aa916bf37df86ba29a3ad18a10a31a5
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-04-14 18:07:23 -0400

    Worked on about section
    Adding cards + fontawesome

--- client/src/components/Page.vue
+++ client/src/components/Page.vue
@@ -16,21 +16,84 @@ export default {
       id="home"
       class="flex items-center justify-center h-screen bg-white dark:bg-gray-900"
     >
-    <div>
-          <p
-            class="inline-block animate-typewriter w-0 overflow-hidden whitespace-nowrap border-r text-9xl"
-          >
-            Hi, I'm Daniel.
-          </p>
-          <p class="text-2xl font-medium">Passionate in software engineering</p>
-        </div>
+      <div>
+        <p
+          class="inline-block animate-typewriter w-0 overflow-hidden whitespace-nowrap border-r text-9xl"
+        >
+          Hi, I'm Daniel.
+        </p>
+        <p class="text-2xl font-medium">Passionate in software engineering</p>
+      </div>
     </div>
     <div
       id="about"
-      class="flex items-center justify-center h-screen bg-white dark:bg-gray-900"
+      class="flex flex-col items-center justify-center h-screen bg-white dark:bg-gray-900"
     >
       <p class="text-8xl font-medium">About</p>
-      <p class="text-2xl font-medium">I am a software engineer with a passion for building web applications.</p>
+      <p class="text-2xl font-medium">
+        I am a software engineer with a passion for building web applications.
+      </p>
+
+      <div class="grid grid-cols-4">
+        <div>
+          <a
+            href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Java
+            </h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              JavaScript
+            </h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              CSS
+            </h5>
+            <i class="fa-brands fa-css3 fa-3x"></i>
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              HTML
+            </h5>
+            <i class="fab fa-html5 fa-3x"></i>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
