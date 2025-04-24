a433176c0d94594135f4f809e4823ba2c3df8b03
Daniel Amoruso <103326339+positivevibes123@users.noreply.github.com>
2025-04-23 21:44:30 -0400

    Update vite.config.js
    Vite config now uses repo name as base

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: "/PortfolioSite/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
